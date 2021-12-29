9af4cf3b981328528e134bd85fdd7287ca840d0f
Paul Kelleher <gitlab@pknw1.co.uk>
2021-12-29 20:54:02 +0000

    app location workflow

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+        <input type="text" value={this.state.value} name="test" />
+  );
 }
 
 export default App;
+
+
+
+/*function App() {
+  const value = 'World';
+  return (
+    <form onSubmit={this.handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
+export default App;
+|*/
